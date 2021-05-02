fbcdac3ee8ea4ceb0ed5c6fd3b19e06d3a1924eb
Josh Jaslow <jbossjaslow@gmail.com>
2021-05-02 16:05:50 -0400

    Added request functionality to api resources

--- Sources/PokeSwift/Model/Utility/Common Models/APIResource.swift
+++ Sources/PokeSwift/Model/Utility/Common Models/APIResource.swift
@@ -9,3 +9,19 @@ public struct APIResource<Type: BaseResourceProtocol>: BaseResourceProtocol {
 	/// The URL of the referenced resource.
 	public let url: String
 }
+
+extension APIResource {
+	func request(completion: @escaping (_ result: Type?) -> Void) {
+		SessionManager.makeRequest(url: url) { (_ result: Result<Type, APIError>) in
+			switch result {
+				case .success(let requestedResult):
+					completion(requestedResult)
+					return
+				case .failure(let error):
+					print(error.localizedDescription)
+					completion(nil)
+					return
+			}
+		}
+	}
+}
